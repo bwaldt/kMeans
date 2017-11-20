f187c816863a609ffc389d5ccdda32343f4afbba
bwaldt <jbrooke@seas.upenn.edu>
2017-11-19 22:04:52 -0500

    run overnight

--- P3_v1.py
+++ P3_v1.py
@@ -23,6 +23,16 @@ def readInFile():
     print np.shape(a)
     np.save('mat.npy', a)
 
+
+def randomSeeding(mat_full,numCenters):
+    users = np.shape(mat_full)[0]
+    print users
+    samples = np.random.randint(users, size=numCenters)
+    return mat_full[samples,:]
+
+
+
+
 def seeding_kmeans_plusplus(clusters, batchSize,mat_full):
 
     users = np.shape(mat_full)[0]
@@ -50,51 +60,68 @@ def seeding_kmeans_plusplus(clusters, batchSize,mat_full):
     return centers
     
 
-def kmeans(centerPoints,mat_full,iterations,batchSize):
-    numCenters = np.shape(centerPoints)[0]
-    metrics = np.zeros((3,iterations))
-    for i in range(iterations):
-        users = np.shape(mat_full)[0]
-	samples = np.random.randint(users, size=batchSize)
-        mat = mat_full[samples,:]
-	d = np.empty([np.shape(mat)[0], numCenters])
-	d.fill(99999)
-	
-	# Calculate Distance of points to each Clusters
-	for idx in range(np.shape(centerPoints)[0]):
-    	    d[:,idx] =  np.linalg.norm(centerPoints[idx,:] - mat,axis=1)**2
+def kmeans(numCenters,mat_full,iterations,batchSize):
 
-	clusterAssignments = np.argmin(d , axis =1) # Assign Points to clusters
+    metrics = np.zeros((6,iterations))
+    for j in range(2):
+	if j == 0:
+	    centerPoints = seeding_kmeans_plusplus(numCenters,batchSize,mat_full)
+	else:
+	    centerPoints = randomSeeding(mat_full,numCenters)
+        
+	for i in range(iterations):
+	    users = np.shape(mat_full)[0]
+	    samples = np.random.randint(users, size=batchSize)
+	    mat = mat_full[samples,:]
+	    d = np.empty([np.shape(mat)[0], numCenters])
+	    d.fill(99999)
+		
+	    # Calculate Distance of points to each Clusters
+	    for idx in range(np.shape(centerPoints)[0]):
+	        d[:,idx] =  np.linalg.norm(centerPoints[idx,:] - mat,axis=1)**2
 
-	for cluster in range(numCenters):
-	    if cluster in clusterAssignments.tolist():
-    	        centerPoints[cluster,:] = np.mean(mat[np.where(clusterAssignments == cluster),:],axis=1) #update center
+	    clusterAssignments = np.argmin(d , axis =1) # Assign Points to clusters
 
-	# Calculate Min/Max/Mean to new Clusters
-	#newD = np.empty([np.shape(mat)[0], len(centers)])                          
-        #newD.fill(99999)
-        	
-        #for idx in range(np.shape(centerPoints)[0]):
-        #    newD[:,idx] =  np.linalg.norm(centerPoints[idx,:] - mat,axis=1)**2
-	distances = np.min(d,axis=1)
-	metrics[0,i] = np.mean(distances)
-	metrics[1,i] = np.amin(distances)
-	metrics[2,i] = np.amax(distances)
+	    d_l2 = np.empty([np.shape(mat)[0], numCenters])
+	    d_l2.fill(99999)
+		
+	    for idx in range(np.shape(centerPoints)[0]):
+	        d_l2[:,idx] = np.linalg.norm(centerPoints[idx,:] - mat,axis =1)
+
+
+            for cluster in range(numCenters):
+	        if cluster in clusterAssignments.tolist():
+	            centerPoints[cluster,:] = np.mean(mat[np.where(clusterAssignments == cluster),:],axis=1) #update center
+
+	    # Calculate Min/Max/Mean to new Clusters
+	    #newD = np.empty([np.shape(mat)[0], len(centers)])                          
+            #newD.fill(99999)
+			
+	    #for idx in range(np.shape(centerPoints)[0]):
+		#    newD[:,idx] =  np.linalg.norm(centerPoints[idx,:] - mat,axis=1)**2
+	    distances = np.min(d_l2,axis=1)
+	    metrics[0+j,i] = np.mean(distances)
+            metrics[2+j,i] = np.amin(distances)
+            metrics[4+j,i] = np.amax(distances)
 
     plotMetrics(metrics,numCenters)
-
+    np.save('data/metrics' + str(numCenters) + '.npy',metrics)
 
 def plotMetrics(metrics,numCenters):
     length = np.shape(metrics)[1]
     x = np.linspace(0,length,length)
 
-    plt.plot(x,metrics[0,:].T,'r--',label="Mean") #CONTAINS YOUR 3RD ROW
-    plt.plot(x,metrics[1,:].T,'g--',label="Min") #CONTAINS YOUR 4TH ROW
-    plt.plot(x,metrics[2,:].T,'b--',label="Max") #CONTAINS YOUR 5TH ROW
+    plt.plot(x,metrics[0,:].T,'b--',label="Mean k-means++") #CONTAINS YOUR 3RD ROW
+    
+    plt.plot(x,metrics[1,:].T,'g--',label="Mean Random") #CONTAINS YOUR 3RD ROW
+    plt.plot(x,metrics[2,:].T,'r--',label="Min k-means++") #CONTAINS YOUR 4TH ROW
+    plt.plot(x,metrics[3,:].T,'c--',label="Min Random") #CONTAINS YOUR 5TH ROW
+    plt.plot(x,metrics[4,:].T,'m--',label="Max k-means++") #CONTAINS YOUR 4T
+    plt.plot(x,metrics[5,:].T,'y--',label="Max Random") #CONTAINS YOUR 5TH R    
     plt.legend(loc='best')
     plt.xlabel("Iterations")
     plt.ylabel("Distance")
-    plt.savefig("metrics" + str(numCenters) + ".jpg")
+    plt.savefig("metricsB" + str(numCenters) + ".jpg")
     plt.close()
 
 
@@ -105,7 +132,6 @@ if __name__ == "__main__":
     mat_full = np.load('copy/mat.npy')
     numOfCenters = [5,50,100,200,300,400,500]
     for k in numOfCenters:
-	centers = seeding_kmeans_plusplus(k,500000,mat_full)
-    	kmeans(centers,mat_full,100,500000)
+	kmeans(k,mat_full,50,2500)
 
 
